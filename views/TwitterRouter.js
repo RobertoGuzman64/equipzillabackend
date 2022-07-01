775990b2d942bc8c16e9c7a91703d5de2b35a143
RobertoGuzman <robertoguzman64@gmail.com>
2022-07-01 13:32:03 +0200

    feat: Deployado el proyecto en Heroku y añadidas las Url en los endpoints en TwitterRouter.js

--- views/TwitterRouter.js
+++ views/TwitterRouter.js
@@ -4,9 +4,15 @@ const TwitterController = require('../controllers/TwitterController');
 
 
 router.get('/buscaTweets', TwitterController.buscaTweets);
+// http://localhost:5000/twitter/buscaTweets
+// https://equipzillabackend.herokuapp.com/twitter/buscaTweets
 
 router.post('/guardaTweets', TwitterController.guardaTweets);
+// http://localhost:5000/twitter/guardaTweets
+// https://equipzillabackend.herokuapp.com/twitter/guardaTweets
 
 router.post('/guarda1Tweet', TwitterController.guarda1Tweet);
+// http://localhost:5000/twitter/guarda1Tweet
+// https://equipzillabackend.herokuapp.com/twitter/guarda1Tweet
 
 module.exports = router;
